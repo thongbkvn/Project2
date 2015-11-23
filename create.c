7f7b5ed34cfd4cb7fb74c0f65777df32d8aa8de3
Pham Van Thong <thongbkvn@gmail.com>
2015-11-23 15:50:15 +0700

    v2

--- create.c
+++ create.c
@@ -3,7 +3,7 @@
 #include <time.h>
 #include <string.h>
 
-#define FILE_NAME "data1.txt"
+#define FILE_NAME_DEFAULT "data1.txt"
 #define P 4
 
 void createData(int m, int n, char* filename)
@@ -11,17 +11,18 @@ void createData(int m, int n, char* filename)
     FILE *fp = fopen(filename, "w");
     fprintf(fp, "%d %d\n", m, n);
     fprintf(fp, "\n");
+    srand(time(NULL));
     for (int i=0; i<m; i++)
     {
-	srand(time(NULL));
-	int p = rand() % 4 + 1;
+	int p = rand() % P + 1;
+	int k;
 	for (int j=0; j<n; j++)
 	{
-	    srand(time(NULL));
-	    int k = rand() % p;
-	    //srand(time(NULL));
-	    if (k == 1)
+	    k = rand() % p;
+	    if (k == 0)
+	    {
 		fprintf(fp, "%4d", rand() % 6);
+	    }
 	    else
 		fprintf(fp, "%4d", 0);
 	}
@@ -34,6 +35,11 @@ int main(int argc, char** argv)
 {
     int row = atoi(argv[1]);
     int col = atoi(argv[2]);
-    createData(row, col, FILE_NAME);
+    char* file_name;
+    if (argc < 4)
+	file_name = FILE_NAME_DEFAULT;
+    else
+	file_name = argv[3];
+    createData(row, col, file_name);
     return 0;
 }
--- makefile
+++ makefile
@@ -1,3 +1,5 @@
-all: test
+all: test create
 test: test.c
 	gcc -fopenmp test.c -o test -lm
+create: create.c
+	gcc create.c -o create
